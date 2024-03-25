b8af7a6a0fd2b4cbde71036858961ee127dc4180
ZL85 <zongliang0726@163.com>
2024-03-25 23:12:31 +0800

    update

--- code/chapter_11/src/learnjava_11/MapTest.java
+++ code/chapter_11/src/learnjava_11/MapTest.java
@@ -6,34 +6,35 @@ import java.util.*;
 
 public class MapTest {
     @Test
-    public void test1(){
+    public void test1() {
         Map map = new HashMap();
 
-        map.put(null,null);
+        map.put(null, null);
 
         System.out.println(map);
     }
+
     @Test
-    public void test2(){
+    public void test2() {
         Map map = new Hashtable();
 
 //        map.put("a",null); //NullPointerException
 //        map.put(null,1); //NullPointerException
-        map.put("a",1);
+        map.put("a", 1);
 
         System.out.println(map);
     }
 
     @Test
-    public void test3(){
+    public void test3() {
         LinkedHashMap map = new LinkedHashMap();
 
-        map.put("Tom",23);
-        map.put("a",new Date());
-        map.put(1,"b");
+        map.put("Tom", 23);
+        map.put("a", new Date());
+        map.put(1, "b");
 
         System.out.println(map);
     }
 
-
+    
 }
